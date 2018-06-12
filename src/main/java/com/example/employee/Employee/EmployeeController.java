eec6be2480781346c5d00821c56d96e743cdbf2a
zx <1374040664@qq.com>
2018-06-12 12:25:46 +0800

    update EmployeeController.java

--- src/main/java/com/example/employee/Employee/EmployeeController.java
+++ src/main/java/com/example/employee/Employee/EmployeeController.java
@@ -20,27 +20,4 @@ public class EmployeeController {
         return employeeService.getEmployeeList();
     }
 
-    /*@ResponseBody
-    @PostMapping(value = "/employees")
-    public String addEmployee ( @RequestParam("id") Integer id,
-                                @RequestParam("name") String name,
-                                @RequestParam("age") Integer age,
-                                @RequestParam("gender") String gender) {
-        Employee employee = new Employee();
-        employee.setId(id);
-        employee.setName(name);
-        employee.setAge(age);
-        employee.setGender(gender);
-        employees.put(id,employee);
-        return "success";
-    }
-
-    @RequestMapping("/")
-    public ModelAndView index(){
-        List<Employee> userList =new ArrayList<Employee>(employees.values());
-
-        ModelAndView modelAndView = new ModelAndView("/index");
-        modelAndView.addObject("userList", userList);
-        return modelAndView;
-    }*/
 }
